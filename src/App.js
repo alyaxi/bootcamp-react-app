65161a81c473863b859569aaec33b3107d6c848f
Syed Ali Uz Zaman <syedaliuzzaman@gmail.com>
2020-07-03 15:19:25 -0700

    update day

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 import {Message} from './Message.js';
 function App() {
   let [count, setcount] = useState(1)
+  let [isDay, setDay] = useState(false)
   return (
-    <div>
+    <div className={`box ${isDay ? 'dayLight' : ''}`}>
+      <h1> Hello Ali Good {isDay ? 'Morning' : 'Night'} </h1>
       <Message counter={count} />
       <br />
       <button onClick={()=>setcount(++count)}>Update Counter</button>
+      <button onClick={()=>{setDay(!isDay) }}>Update Day</button>
     </div>
   );
 }
