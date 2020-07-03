05ecc5debd926de570749b8c38cd4fce15782dca
Syed Ali Uz Zaman <syedaliuzzaman@gmail.com>
2020-07-03 11:04:56 -0700

    Updated Counter

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, {useState} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-export default function App() {
-  let [Count, setCount] = useState(1)
+function App() {
+  let [Count, setCount] = useState(0)
   return (
     <div>
       <h3>Value of counter vairable is {Count} </h3>
       <br />
-      <button onClick={()=>{alert("Button Clicked")}}>Update Counter</button>
+      <button onClick={()=>setCount(++Count)}>Update Counter</button>
     </div>
   );
 }
-
 export default App;
