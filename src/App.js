1e1c39e896d39646c8e92ddd133ec6a01324c4ad
Syed Ali Uz Zaman <syedaliuzzaman@gmail.com>
2020-07-03 10:13:33 -0700

    Made a Counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  let [Count, setCount] = useState(1)
   return (
     <div>
-     <h1>Hello Syed Ali Uz Zaman</h1>
-     <p>This is from Pakistan</p>
-     <h1>
-       Hello Pakistan
-     </h1>
+      <h3>Value of counter vairable is {Count} </h3>
+      <br />
+      <button onClick={()=>{alert("Button Clicked")}}>Update Counter</button>
     </div>
   );
 }
